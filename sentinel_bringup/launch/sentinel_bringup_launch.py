556bfc7655f6ac767995afe5676c3af0b814aadb
Jarunyawat Buasri <jarunyawat.b@gmail.com>
2025-05-25 13:14:08 +0700

    add bringup

---
+++ sentinel_bringup/launch/sentinel_bringup_launch.py
@@ -0,0 +1,29 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import PathJoinSubstitution
+from launch_ros.substitutions import FindPackageShare
+
+def generate_launch_description():
+
+    description_launch = IncludeLaunchDescription(
+        PathJoinSubstitution([
+            FindPackageShare('sentinel_description'),
+            'launch',
+            'description_launch.py'
+        ])
+    )
+    
+    micro_ros_node = Node(
+        package='micro_ros_agent',
+        executable='micro_ros_agent',
+        name='micro_ros_agent',
+        output='screen',
+        arguments=['serial', '--dev', '/dev/ttyACM0', '-b', '1500000'],
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(description_launch)
+    ld.add_action(micro_ros_node)
+
+    return ld
